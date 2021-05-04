522b9b02d4dd91fc88c9d0bf9159340f6db86852
unknown <bakewelljake@gmail.com>
2021-05-04 09:19:24 -0500

    Bubble Sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,6 @@
 function bubbleSort(array) {
     for (let j = 0; j < array.length; j++) {
-        for (let i = 0; i < array.length; i++) {
+        for (let i = 0; i < array.length - j; i++) {
             if (array[i] > array[i + 1]) {
                 let temp = array[i];
                 array[i] = array[i + 1];
