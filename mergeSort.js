0f943015994a1121f36c730a3f3efbc303f0ad7d
unknown <bakewelljake@gmail.com>
2021-05-06 09:36:51 -0500

    Merge Sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,36 +1,35 @@
-const combine = (arr1, arr2)=>{
-    if (arr1.length > arr2.length) {
-        var longLen = arr1.length;
-        var shortLen = arr2.length;
-        var len = shortLen + longLen;
-        var longArr = arr1;
-    }
-    else {
-        var longLen = arr2.length;
-        var shortLen = arr1.length;
-        var len = shortLen + longLen;
-        var longArr = arr2;
-    }
-    let leftArrInc = 0;
-    let rightArrInc = 0;
-    let combinedArr = [];
-    for (let i = 0; i < len; i++) {
-        if (arr1[leftArrInc] > arr2[rightArrInc]) {
-            combinedArr.push(arr2[rightArrInc]);
-            rightArrInc += 1;
-        }
-        else if (arr1[leftArrInc] < arr2[rightArrInc]) {
-            combinedArr.push(arr1[leftArrInc]);
-            leftArrInc += 1;
-        }
-        else if (i >= shortLen) {
-            combinedArr.push(longArr[rightArrInc]);
-            rightArrInc += 1;
+const combine = (arr1, arr2) => {
+    var result = [];
+    var i = 0;
+    var j = 0;
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]> arr2[j]){
+            result.push(arr2[j])
+            j++
+        }else{
+            result.push(arr1[i])
+            i++
         }
     }
-    return combinedArr;
+    while(i<arr1.length){
+        result.push(arr1[i])
+        i++
+    }
+    while(j<arr2.length){
+        result.push(arr2[j])
+        j++
+    }
+    console.log(result);
+    return result;
 }
 
+const mergeSort = (arr) => {
+    const halfLen = Math.floor(arr.length / 2);
+    if (arr.length < 2) {
+        return arr;
+    }
+    const leftSide = arr.splice(0, halfLen);
+    return combine(mergeSort(leftSide), mergeSort(arr));
+}
 
-
-console.log(combine([4,6,7], [1,3,6,9]));
+console.log(mergeSort([8,4,1,3,5,6,9,7]))
