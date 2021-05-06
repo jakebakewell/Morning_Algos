52e3238384d5bcbb30454ba23c75fb2ba1e0f967
unknown <bakewelljake@gmail.com>
2021-05-06 09:40:28 -0500

    Merge Sort changed conditional

--- mergeSort.js
+++ mergeSort.js
@@ -25,7 +25,7 @@ const combine = (arr1, arr2) => {
 
 const mergeSort = (arr) => {
     const halfLen = Math.floor(arr.length / 2);
-    if (arr.length < 2) {
+    if (arr.length == 1) {
         return arr;
     }
     const leftSide = arr.splice(0, halfLen);
